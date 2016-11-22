edbd079dfb8ed65cd947ba1c2adadcc43b45807f
jetpackpony <jetpackpony@gmail.com>
2016-11-22 20:09:55 +0300

    Make the server available on the network

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,8 @@ module.exports = function(grunt) {
         port: 9000,
         livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        //hostname: 'localhost'
+        hostname: '192.168.1.35'
       },
       livereload: {
         options: {
