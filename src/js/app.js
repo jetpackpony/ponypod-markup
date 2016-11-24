f2a2a75e50006258b9cbc4cac1274d330679124c
jetpackpony <jetpackpony@gmail.com>
2016-11-24 15:13:30 +0300

    Add js to manipulate play/pause buttons

--- src/js/app.js
+++ src/js/app.js
@@ -12,18 +12,44 @@ $(function() {
   });
   $('body > .shadow').on('click', function() {
     $('body').removeClass('menu-open');
+    e.preventDefault();
   });
   $('#toggle-menu').on('click', function() {
     $('body').addClass('menu-open');
+    e.preventDefault();
   });
 
   // Toggle podcast details
   $('.toggle-button').on('click', function() {
     $('.podcast-details').toggleClass('expanded');
+    e.preventDefault();
   });
 
   // Toggle options menu
   $('#options-menu').on('click', function() {
     $('.options-list').toggleClass('hidden');
+    e.preventDefault();
+  });
+
+  // Toggle play pause
+  $('a.play').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('now-playing');
+    if ($(this).parent('.episode-line')) {
+      $('#player').removeClass('hidden');
+    }
+  });
+
+  // Go to episode page
+  $('.episode-line').on('click', function() {
+    window.location.href = "/episode.html";
+  });
+
+  // Expand the player
+  $('.player-collapsed .episode').on('click', function() {
+    $('#player').removeClass('hidden');
+    //$('#player').removeClass('player-collapsed');
+    $('#player').addClass('player-expanded');
   });
 });
