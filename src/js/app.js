e78405e1846e177a362cb53e66db4e7ac931eb20
jetpackpony <jetpackpony@gmail.com>
2016-11-25 22:24:19 +0300

    Add sliding side menu and shadow

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,26 @@ $(function() {
     $('.nav-search input').val('');
   });
 
+  /*
+   * Side-menu JS
+   */
+  $(document).swipe(function(e, dx, dy) {
+    console.log("swipe");
+    if (Math.abs(dx) < Math.abs(dy) * 10 ) {
+      return;
+    }
+    if(dx < 0){
+      $('body').removeClass('menu-open');
+    } else {
+      $('body').addClass('menu-open');
+    }
+  });
+  $('#body-shadow').on('click', function() {
+    $('body').removeClass('menu-open');
+  });
+  $('#toggle-menu').on('click', function() {
+    $('body').addClass('menu-open');
+  });
 
 
 });
