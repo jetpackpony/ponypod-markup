f720059608855dc5ecd32880c53dab4dc250793e
jetpackpony <jetpackpony@gmail.com>
2016-11-25 21:41:02 +0300

    Add a navigation bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
 $(function() {
+  /*
+   * Nav bar JS
+   */
+  $('#open-search').click(function() {
+    $('.nav-wrapper').addClass('search-open');
+    $('.nav-search input').focus();
+  });
+  $('#close-search').click(function() {
+    $('.nav-wrapper').removeClass('search-open');
+    $('.nav-search input').val('');
+  });
+
+
+
 });
